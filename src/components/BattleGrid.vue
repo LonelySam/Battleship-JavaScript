ed7553790b4ddc55367ba019ccf170431f9f92ba
unknown <soledad_2012@hotmail.com>
2018-08-02 01:19:17 -0400

    Relation between form and creation of table implemented

--- src/components/BattleGrid.vue
+++ src/components/BattleGrid.vue
@@ -1,11 +1,48 @@
 <template>
   <div class="grid">
+    <table id="grid-table">
+    </table>
   </div>
 </template>
 
 <script>
+import { EventBus } from '@/services/event-bus';
+
 export default {
   name: 'BattleGrid',
+  data() {
+    return {
+      width: 0,
+      height: 0,
+    };
+  },
+  mounted() {
+    EventBus.$on('create-form', (width, height) => {
+      if (width !== undefined) {
+        this.width = width;
+      }
+      if (height !== undefined) {
+        this.height = height;
+      }
+      console.log(this.width);
+      console.log(this.height);
+      this.generateTable();
+    });
+  },
+  methods: {
+    generateTable() {
+      const table = document.getElementById('grid-table');
+      let contentTable = '';
+      for (let i = 0; i < this.height; i++) {
+        contentTable += '<tr>';
+        for (let j = 0; j < this.width; j++) {
+          contentTable += '<td>*</td>';
+        }
+        contentTable += '</tr>';
+      }
+      table.innerHTML = contentTable;
+    },
+  },
 };
 </script>
 
--- src/components/FormCreate.vue
+++ src/components/FormCreate.vue
@@ -1,15 +1,27 @@
 <template lang="html">
   <div class="form">
-    <input type="text" name="" value="">
-    <input type="text" name="" value="">
-    <button type="button" name="button">Create</button>
-    <input type="text" name="" value="">
+    <input type="text" name="game" v-model="width">
+    <input type="text" name="game" v-model="height">
+    <button type="button" name="game" @click="sendValue(width, height)">Create</button>
   </div>
 </template>
 
 <script>
+import { EventBus } from '@/services/event-bus';
+
 export default {
   name: 'FormCreate',
+  data() {
+    return {
+      height: 0,
+      width: 0,
+    };
+  },
+  methods: {
+    sendValue(width, height) {
+      EventBus.$emit('create-form', width, height);
+    },
+  },
 };
 </script>
 
